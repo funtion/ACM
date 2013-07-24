b564f74b2442971742e76a3c8e36655ac130a902
funtion <xieshufang76@gmail.com>
2013-07-24 21:08:10 +0800

    Create 快速卷积.cpp

---
+++ 模板/快速卷积.cpp
@@ -0,0 +1,114 @@
+//http://www.csdn123.com/html/itweb/20130724/18605_18621_18636.htm
+//http://acm.hdu.edu.cn/showproblem.php?pid=4609
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <math.h>
+#include <complex>
+using namespace std;
+typedef complex<double> Complex;
+const double PI = acos(-1.0);
+void change(Complex y[],int len)
+{
+    int i,j,k;
+    for(i = 1, j = len/2;i < len-1;i++)
+    {
+        if(i < j)swap(y[i],y[j]);
+        k = len/2;
+        while( j >= k)
+        {
+            j -= k;
+            k /= 2;
+        }
+        if(j < k)j += k;
+    }
+}
+void fft(Complex y[],int len,int on)
+{
+    change(y,len);
+    for(int h = 2;h <= len;h <<= 1)
+    {
+        Complex wn(cos(-on*2*PI/h),sin(-on*2*PI/h));
+        for(int j = 0;j < len;j += h)
+        {
+            Complex w(1,0);
+            for(int k = j;k < j+h/2;k++)
+            {
+                Complex u = y[k];
+                Complex t = w*y[k+h/2];
+                y[k] = u+t;
+                y[k+h/2] = u-t;
+                w = w*wn;
+            }
+        }
+    }
+    if(on == -1)
+        for(int i = 0;i < len;i++)
+            y[i].real() /= len;
+}
+
+const int MAXN = 400040;
+Complex x1[MAXN];
+int a[MAXN/4];
+long long num[MAXN];//100000*100000会超int
+long long sum[MAXN];
+
+int main()
+{
+    int T;
+    int n;
+    scanf("%d",&T);
+    while(T--)
+    {
+        scanf("%d",&n);
+        memset(num,0,sizeof(num));
+        for(int i = 0;i < n;i++)
+        {
+            scanf("%d",&a[i]);
+            num[a[i]]++;
+        }
+        sort(a,a+n);
+        int len1 = a[n-1]+1;
+        int len = 1;
+        while( len < 2*len1 )len <<= 1;
+        for(int i = 0;i < len1;i++)
+            x1[i] = Complex(num[i],0);
+        for(int i = len1;i < len;i++)
+            x1[i] = Complex(0,0);
+        fft(x1,len,1);
+        for(int i = 0;i < len;i++)
+            x1[i] = x1[i]*x1[i];
+        fft(x1,len,-1);
+        for(int i = 0;i < len;i++)
+            num[i] = (long long)(x1[i].real()+0.5);
+        //num里面是卷积后的数组
+        len = 2*a[n-1];
+        //减掉取两个相同的组合
+        for(int i = 0;i < n;i++)
+            num[a[i]+a[i]]--;
+        //选择的无序，除以2
+        for(int i = 1;i <= len;i++)
+        {
+            num[i]/=2;
+        }
+        sum[0] = 0;
+        for(int i = 1;i <= len;i++)
+            sum[i] = sum[i-1]+num[i];
+        long long cnt = 0;
+        for(int i = 0;i < n;i++)
+        {
+            cnt += sum[len]-sum[a[i]];
+            //减掉一个取大，一个取小的
+            cnt -= (long long)(n-1-i)*i;
+            //减掉一个取本身，另外一个取其它
+            cnt -= (n-1);
+            //减掉大于它的取两个的组合
+            cnt -= (long long)(n-1-i)*(n-i-2)/2;
+        }
+        //总数
+        long long tot = (long long)n*(n-1)*(n-2)/6;
+        printf("%.7lf\n",(double)cnt/tot);
+    }
+    return 0;
+}
