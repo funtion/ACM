62dc8558c7ea717e843b840a2238d7d9e63da62f
funtion <xieshufang76@gmail.com>
2013-09-28 15:24:40 +0800

    Update KMP.cpp

--- 模板/KMP.cpp
+++ 模板/KMP.cpp
@@ -1,3 +1,4 @@
+//automachine
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -47,3 +48,54 @@ int main()
     }
     return 0;
 }
+/////////////fial/////////////
+#include <iostream>
+#include <string>
+using namespace std;
+
+int fail[1000005];
+
+int kmp (const string& s1, const string& s2, int n) {
+    fail[0] = 0;
+    for (int i=1; i<=n; ++i) {
+        int i1 = fail[i-1];
+        while (i1!=0 && s1[i1] != s1[i-1]) i1 = fail[i1];
+
+        if (s1[i1] == s1[i-1] && i!=1) fail[i] = i1+1;
+        else fail[i] = i1;
+    }
+    //cout << s1 << endl;
+    //for (int i=0; i!=n; ++i) cout << fail[i] << ' '; cout << endl;
+    int j=0, k=0;
+    while (j!=n) {
+        if (s1[k] == s2[j]) {
+            ++j; ++k;
+        } else {
+            while (s1[k] != s2[j] && k!=0) k = fail[k];
+            if (s1[k] == s2[j]) ++k;
+            ++j;
+        }
+    }
+    return k;
+}
+
+int n;
+int main()
+{
+    //cout << kmp("abcDabcEabcDabcF", "", 16);
+    while (cin>>n)
+    {
+        for (int i=0; i!=n; ++i) {
+            string s;
+            cin >> s;
+            string s1 = s.substr(0, s.length()/2);
+            string s2 = s.substr(s.length() - s.length()/2, s.length()/2);
+            //cout << s1 << '\t' << s2 << endl;
+
+            cout << kmp(s1,s2,s1.length()) << endl;
+
+        }
+    }
+}
+////////////////////////////
+
