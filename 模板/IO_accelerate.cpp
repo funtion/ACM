8013e2acad5acd07418bbacf5e8b1e57474bc22a
funtion <xieshufang76@gmail.com>
2013-09-30 22:30:05 +0800

    Update IO_accelerate.cpp

--- 模板/IO_accelerate.cpp
+++ 模板/IO_accelerate.cpp
@@ -1,2 +1,27 @@
 std::ios::sync_with_stdio(false); 
 //cin 瞬间拥有scanf的速度
+//faster
+namespace IO
+{
+template<class T> inline T RD(T &x)
+{
+    char c;
+    for (c = getchar(); c < '-'; c = getchar());
+    if (c == '-')
+    {
+        x = '0' - getchar();
+        for (c = getchar(); '0' <= c && c <= '9'; c = getchar()) x = x * 10 + '0' - c;
+    }
+    else
+    {
+        x = c - '0';
+        for (c = getchar(); '0' <= c && c <= '9'; c = getchar()) x = x * 10 + c - '0';
+    }
+    return x;
+} inline int RD()
+{
+    int x;
+    return RD(x);
+}
+} using namespace IO;
+//
